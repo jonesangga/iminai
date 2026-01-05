2dece9d110cf1e379ec50c47b98f8a9bdfade7a8
jonesangga <jonesangga@proton.me>
2026-01-05 10:59:47 +0700

    fix translate test

--- tests/unit/translate.lua
+++ tests/unit/translate.lua
@@ -1,32 +1,16 @@
-local lust = require("libraries/lust")
-local main = require("main")
 local vocab = require("vocab")
 local en = require("imola_en")
-local inspect = require("libraries/inspect")
 
-local describe, it, expect = lust.describe, lust.it, lust.expect
-
-local text = ""
-local function printfn(s)
-    text = text .. tostring(s) .. '\n'
-end
-
-local function writefn(s)
-    text = text .. tostring(s)
-end
-
-main.setup(printfn, writefn)
-
-local function runner(src, output)
-    text = ""
-    main.run_string(src)
-    expect( text ).to.diff( output )
-end
-
-describe("all vocabs", function()
-    for k, _ in pairs(vocab) do
-        it(k, function()
-            runner("!en " .. k, en[k] .. '\n')
-        end)
+for k, _ in pairs(vocab) do
+    if not en[k] then
+        error("FAIL " .. k .. " not found in en")
     end
-end)
+end
+
+for k, _ in pairs(en) do
+    if not vocab[k] then
+        error("FAIL " .. k .. " not found in vocab")
+    end
+end
+
+print("PASS translate")
